faeaee79ff4e3170a1c15680c5f02c4c52144075
LIUZHAO <liuzhaomax@163.com>
2024-04-11 19:55:15 +0800

    header

--- src/page/demo/handlers.js
+++ src/page/demo/handlers.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 import { URL } from "../../config/url"
+import short from "short-uuid"
 
 export const getDemo = () => {
+    axios.defaults.headers.common["Request_id"] = short().new()
     return axios.get(URL.INNER.Demo)
 }
--- src/page/home/handlers.js
+++ src/page/home/handlers.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 import { URL } from "../../config/url"
+import short from "short-uuid"
 
 export const getContributors = () => {
+    axios.defaults.headers.common["Request_id"] = short().new()
     return axios.get(URL.OUTER.Contributors)
 }
